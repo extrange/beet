42f836c3df21918e0fcfe56ed00b2240761dd64c
extrange <29305375+extrange@users.noreply.github.com>
2024-04-16 15:03:25 +0800

    reorganize

--- scripts/check_exists.py
+++
@@ -1,9 +0,0 @@
-# Check if all the files in a list exist
-
-from pathlib import Path
-
-with open("playlists_txt/chanel.m3u") as f:
-    for line in f.readlines():
-        p = Path(line.strip())
-        if not p.exists():
-            print(p)
